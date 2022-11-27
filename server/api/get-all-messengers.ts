0600c44ca8c35de54cab278044dd2baf90d94a3b
许圣峰 <kricsleo@163.com>
2022-11-27 21:22:00 +0800

    fix: fix missing import

--- server/api/get-all-messengers.ts
+++ server/api/get-all-messengers.ts
@@ -1,4 +1,5 @@
 import { getAllMessengers } from '~~/composables/db'
+import { answer } from '~~/utils/utils'
 
 export default defineEventHandler(answer(async event => {
   const messengers = await getAllMessengers()
--- server/api/get-messenger.ts
+++ server/api/get-messenger.ts
@@ -1,3 +1,5 @@
+import { answer } from '~~/utils/utils'
+
 export default defineEventHandler(answer(async event => {
   const messengerId = getQuery(event).id as string
   const messenger = await getMessenger(messengerId)
