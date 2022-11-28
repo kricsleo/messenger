a95eec138a32ea1e3aef0840471ad9aa73fb1933
许圣峰 <kricsleo@163.com>
2022-11-28 18:47:10 +0800

    feat: add messenger list

--- pages/index.vue
+++ pages/index.vue
@@ -4,21 +4,30 @@ import { javascript } from '@codemirror/lang-javascript'
 import { oneDark } from '@codemirror/theme-one-dark'
 import copyToClipboard from 'copy-to-clipboard';
 
-const domain = 'https://messenger.kric.cc'
-// const domain = 'http://localhost:3000'
 const extentions = [javascript({ typescript: true }), oneDark]
-const messengerPrefix = `${domain}/api/messenger/`
+
+const origin = ref('~')
+
+// const origin = 'http://localhost:3000'
+const messengerPrefix = computed(() => `${origin.value}/api/messenger/`)
 const messengerId = ref()
-const messengerUrl = computed(() => messengerPrefix + messengerId.value)
+const messengerUrl = computed(() => messengerPrefix.value + messengerId.value)
 const address = ref()
 const exchanger = ref()
 
 const testData = ref()
 const messegeReply = ref()
 
+const messengerList = ref<Messenger[]>()
+
+onMounted(() => {
+  origin.value = window.location.origin
+  loadMessengerList()
+})
+
 async function saveMessenger() {
   // todo: validate code before submit
-  await $fetch(`${domain}/api/save-messenger`, {
+  await $fetch(`${origin.value}/api/save-messenger`, {
     method: 'POST',
     body: { 
       id: messengerId.value,
@@ -26,6 +35,7 @@ async function saveMessenger() {
       exchanger: exchanger.value
     }
   })
+  loadMessengerList()
 }
 
 async function triggerTest() {
@@ -33,6 +43,17 @@ async function triggerTest() {
     method: 'POST',
     body: JSON.parse(testData.value)
   })
+}
+
+async function loadMessengerList() {
+  const result: any = await $fetch(`${origin.value}/api/get-all-messengers`)
+  messengerList.value = result.d.messengers
+}
+
+function showMessenger(messenger: Messenger) {
+  messengerId.value = messenger.id
+  address.value = messenger.address
+  exchanger.value = messenger.exchanger
 }
 </script>
 
@@ -65,7 +86,7 @@ async function triggerTest() {
     />
 
     <div my-10 x-center>
-      <button class="btn" @click="saveMessenger">save</button>
+      <button class="btn" @click="saveMessenger">Save</button>
     </div>
 
     <div border mt-20 />
@@ -85,7 +106,7 @@ async function triggerTest() {
         />
       </div>
 
-      <button class="btn" @click="triggerTest" shrink-0>test =></button>
+      <button class="btn btn--plain" @click="triggerTest" shrink-0>Test =></button>
 
       <div :style="{width: '44%'}" shrink-0>
         <div y-center flex-wrap my-10>
@@ -103,12 +124,26 @@ async function triggerTest() {
       </div>
     </div>
 
+    <div border mt-20 />
+    <div>
+      <div class="title" my-10> Messengers: </div>
+      <div 
+        v-for="messengerItem in messengerList" 
+        :key="messengerItem.id"
+        flex my-10 justify-between>
+        <span>{{messengerPrefix}}{{messengerItem.id}}</span>
+        <button class="btn btn--plain" @click="showMessenger(messengerItem)">Edit</button>
+      </div>
+    </div>
+
   </section>
 </template>
 
 <style scope>
 .title {
   width: 100px;
+  font-weight: 700;
+  font-size: 16px;
 }
 .input {
   border: 1px solid #444c56;
@@ -127,5 +162,10 @@ async function triggerTest() {
   border: 1px solid rgb(205 217 229 / 10%);
   border-radius: 6px;
   appearance: none;
+}
+.btn--plain {
+  color: #347d39;
+  border-color: currentColor;
+  background-color: transparent;
 }
 </style>
