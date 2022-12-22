4e48cbcf6cb09332470f42751901ff4d0cbc8347
许圣峰 <kricsleo@163.com>
2022-12-22 21:12:09 +0800

    feat: add github link

--- components/Header.vue
+++ components/Header.vue
@@ -5,12 +5,13 @@ import MenuLink from './MenuLink.vue';
 
 <template>
   <header sticky top-0 bg-white dark:bg-dark z-2 shadow-sm shadow-gray-1 dark:shadow-gray-7 normalize-scroll>
-    <div max-w-1044 m-auto px-10 flex items-center>
+    <div max-w-1044 m-auto px-10 flex items-center gap-15 text-20>
       <img src="/favicon.svg" w-40 h-40 rounded-full >
       <!-- <h1 ml-12 text-20 mr-auto>Messenger</h1> -->
       <MenuLink to="/" ml-auto>Messengers</MenuLink>
       <MenuLink to="/playground">Playground</MenuLink>
-      <button @click="toggleDark()" dark:i-carbon-moon i-carbon-sun ml-15 text-20 />
+      <a i-carbon:logo-github href="https://github.com/kricsleo/messenger" target="_blank" />
+      <button @click="toggleDark()" dark:i-carbon-moon i-carbon-sun />
     </div>
   </header>
 </template>
