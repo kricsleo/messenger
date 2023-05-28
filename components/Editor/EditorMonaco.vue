4455c9e6707c6772d6aae0cd3fe65e06e6e0b22d
许圣峰 <kricsleo@163.com>
2023-05-28 22:05:13 +0800

    chore: playground

--- components/Editor/EditorMonaco.vue
+++ components/Editor/EditorMonaco.vue
@@ -69,8 +69,41 @@ onMounted(async () => {
     content: `declare interface Meta { author: string }`,
     filePath: 'types.d.ts'
   }])
+
+  async function promiseRetry<T>(
+  promise: () => Promise<T>,
+  initial_retry_count: number,
+  retry_delay: number,
+): Promise<T> {
+  async function __promiseRetry(promise: () => Promise<T>, remaining_retry_count: number): Promise<T> {
+    try {
+      const res = await promise();
+      return res;
+    } catch (e) {
+      if (remaining_retry_count <= 0) {
+        throw e;
+      }
+      setTimeout(() => {
+        __promiseRetry(promise, remaining_retry_count - 1);
+      }, retry_delay ** (1 + 0.1 * (initial_retry_count - remaining_retry_count + 1)));
+    }
+  }
+
+  return __promiseRetry(promise, initial_retry_count);
+}
+
+// default async load ts will override `wireTmGrammars`
+// so `wireTmGrammars` after default async load
+await promiseRetry(
+  async () => {
+    await monaco.languages.typescript.getTypeScriptWorker();
+  },
+  5,
+  10,
+);
   await wireTmGrammars(monaco, registry, grammars, editor)
   
+  // editor.onDidScrollChange()
   editor.onDidChangeModelContent(() => {
     const value = editor.getValue()
     emits('update:modelValue', value)
